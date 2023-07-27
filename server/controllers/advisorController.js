47cbaee2eade7a8967358a35e21f22083656ace3
Naratip <naratip.18257@gmail.com>
2023-07-27 09:33:57 +0700

    add advisor_detail

--- server/controllers/advisorController.js
+++ server/controllers/advisorController.js
@@ -7,3 +7,20 @@ exports.advisor_list = asyncHandler(async (req, res, next) => {
   const advisors = await Advisor.find().exec();
   res.json({ advisors: advisors });
 });
+
+exports.advisor_detail = asyncHandler(async (req, res, next) => {
+  const [advisor, students] = await Promise.all([
+    Advisor.findById(req.params.id).exec(),
+    Student.find(
+      { advisor: req.params.id },
+      { publications: 0, status: 0, advisor: 0 }
+    ).exec(),
+  ]);
+
+  if (advisor === null) {
+    const err = new Error("Book not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.json({ advisor: advisor, students: students });
+});
