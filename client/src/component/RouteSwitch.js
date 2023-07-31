8cebe0d306db917645db290f4369c6ab95960af5
Naratip <naratip.18257@gmail.com>
2023-07-31 14:45:16 +0700

    add advisors,students list to routeSwitch

--- client/src/component/RouteSwitch.js
+++ client/src/component/RouteSwitch.js
@@ -1,2 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import AdvisorsList from "./AdvisorsList";
+import StudentsList from "./StudentsList";
+
+export default function RouteSwitch() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/students" element={<StudentsList />} />
+        <Route path="/advisors" element={<AdvisorsList />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
