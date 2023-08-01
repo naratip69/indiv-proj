1d0c144177bfbccc67beef5f0f48071839330ec1
Naratip <naratip.18257@gmail.com>
2023-08-01 22:43:55 +0700

    create chart component

---
+++ client/src/component/Chart.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+
+export default function Chart({ chartData }) {
+  return <Line data={chartData} />;
+}
