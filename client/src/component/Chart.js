7db1650888338e3815d6ede990722c600919e2bb
Naratip <naratip.18257@gmail.com>
2023-08-02 14:02:59 +0700

    make chart to display int

--- client/src/component/Chart.js
+++ client/src/component/Chart.js
@@ -3,5 +3,16 @@ import { Chart as ChartJS } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
 export default function Chart({ chartData }) {
-  return <Line data={chartData} />;
+  return (
+    <Line
+      data={chartData}
+      options={{
+        scales: {
+          y: {
+            ticks: { precision: 0 },
+          },
+        },
+      }}
+    />
+  );
 }
